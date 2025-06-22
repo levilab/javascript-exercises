e7be4cad19542842222b8fb357503c55978d480e
sonvu <vuhaison95@gmail.com>
2025-06-22 15:37:19 +0930

    6,7,8 js done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let strings='';
+    if (num < 0) {
+        return "ERROR"
+    } else if (string ==='') {
+        return ''
+    }
 
+    for (let i=1; i<=num ; i++) {
+        strings += string
+    }
+    return strings; 
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+        for (let arg of args) {
+            if (arr.includes(arg)) {
+        const leftIndex = arr.findIndex(item => item == arg)
+        const rightIndex = arr.findLastIndex(item => item == arg)
+        arr.splice(leftIndex, rightIndex - leftIndex + 1)
+            }
+        }
+    return arr       
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// find all the indexes of the
