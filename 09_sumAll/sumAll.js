6ac7e7e181a7cda3af4124d17048e0ebd53f7381
sonvu <vuhaison95@gmail.com>
2025-06-23 10:52:45 +0930

    09 finished

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (start < 0 || end < 0) 
+        {return 'ERROR'};
 
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {return 'ERROR'}
+
+    // if (start % 1 !==0 || end % 1 !==0)
+    //     {return 'ERROR'}
+
+    // if (typeof(start)!=='number' || (typeof(end)!=='number'))
+    //     {return 'ERROR'}
+
+    // let start = (start > end) ? end : start;
+    // let end = (start < end) ? end : start;
+
+    if (start > end) {
+        [start, end] = [end, start]
+    }
+
+    for (i=start; i<=end; i++) {
+        sum+=i
+    }
+    return sum
 };
 
 // Do not edit below this line
