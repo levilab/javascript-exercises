e15b9b1acc700f39f9b9491b8c23bf9a1287bc57
sonvu <vuhaison95@gmail.com>
2025-06-23 09:53:25 +0930

    add other sols using extra memory

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,14 +1,27 @@
-const removeFromArray = function(arr, ...args) {
-        for (let arg of args) {
-            if (arr.includes(arg)) {
-        const leftIndex = arr.findIndex(item => item == arg)
-        const rightIndex = arr.findLastIndex(item => item == arg)
-        arr.splice(leftIndex, rightIndex - leftIndex + 1)
-            }
-        }
-    return arr       
-};
+// const removeFromArray = function(arr, ...args) {
+//         for (let arg of args) {
+//             if (arr.includes(arg)) {
+//         const leftIndex = arr.findIndex(item => item == arg)
+//         const rightIndex = arr.findLastIndex(item => item == arg)
+//         arr.splice(leftIndex, rightIndex - leftIndex + 1)
+//             }
+//         }
+//     return arr       
+// };
 
+// const removeFromArray = function(arr, ...args) {
+//     return arr.filter(item => !args.includes(item))
+// }
+
+const removeFromArray = function(arr, ...args) {
+    const result = []
+    arr.forEach(item => {
+        if (!args.includes(item)) {
+            result.push(item)
+        }
+    })
+    return result
+}
 // Do not edit below this line
 module.exports = removeFromArray;
 
